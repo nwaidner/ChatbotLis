e1597ef5aafd30896bb6f4976adf57cfc9fa1841
Nick Waidner <n.waidner@web.de>
2023-07-21 14:03:01 +0200

    add sentiment analysis for emotions

--- emotion_analysis.py
+++ emotion_analysis.py
@@ -4,8 +4,3 @@ import text2emotion
 def emotion_detection_text2emotion(x):
     all_emotions_value = text2emotion.get_emotion(x)
     return max(zip(all_emotions_value.values(), all_emotions_value.keys()))[1]
-
-
-input_text = "I am sad for you but i am happy"
-result_emotion = emotion_detection_text2emotion(input_text)
-print(f"Emotion: {result_emotion}")
